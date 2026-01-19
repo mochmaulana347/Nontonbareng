d48ff52ef9259a10d053e4344032c09a9e964e7b
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-19 19:48:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,26 +5,51 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-// Server menyimpan status terakhir untuk orang yang baru join
+let users = {};
 let roomState = {
-    url: 'https://www.youtube.com/watch?v=aqz-KE-bpKQ', // Default video
+    url: 'https://www.youtube.com/watch?v=aqz-KE-bpKQ',
     time: 0,
     playing: false,
     updatedAt: Date.now()
 };
 
 io.on('connection', (socket) => {
-    
-    // Saat ada orang baru masuk (Initial Sync)
-    socket.on('join-room', () => {
-        let now = Date.now();
-        let estimatedTime = roomState.time;
+    socket.on('join-room', (name) => {
+        users[socket.id] = { name: name, joinedAt: Date.now() };
         
-        // Jika video sedang jalan, hitung selisih waktu agar sinkron
+        // Hitung estimasi waktu sekarang untuk yang baru join
+        let estimatedTime = roomState.time;
         if (roomState.playing) {
-            estimatedTime += (now - roomState.updatedAt) / 1000;
+            estimatedTime += (Date.now() - roomState.updatedAt) / 1000;
         }
 
+        io.emit('update-users', Object.values(users));
         socket.emit('video-command', {
             action: 'initial-sync',
             url: roomState.url,
+            time: estimatedTime,
+            playing: roomState.playing
+        });
+    });
+
+    socket.on('video-command', (data) => {
+        roomState = {
+            url: data.url || roomState.url,
+            time: data.time,
+            playing: (data.action === 'play'),
+            updatedAt: Date.now()
+        };
+        socket.broadcast.emit('video-command', data);
+    });
+
+    socket.on('new-message', (msg) => {
+        socket.broadcast.emit('chat-receive', { user: users[socket.id]?.name || "Anonim", msg });
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('update-users', Object.values(users));
+    });
+});
+
+http.listen(process.env.PORT || 3000);
