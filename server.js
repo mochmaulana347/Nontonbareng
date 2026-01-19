c84ecf8e3576a0bc469ba511fe5c4ed655c7ab5e
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-19 20:30:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,43 +6,20 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 let users = {};
-let roomState = {
-    url: 'https://www.youtube.com/watch?v=aqz-KE-bpKQ',
-    time: 0,
-    playing: false,
-    updatedAt: Date.now()
-};
 
 io.on('connection', (socket) => {
     socket.on('join-room', (name) => {
-        users[socket.id] = { name: name, joinedAt: Date.now() };
-        
-        let estimatedTime = roomState.time;
-        if (roomState.playing) {
-            estimatedTime += (Date.now() - roomState.updatedAt) / 1000;
-        }
-
+        users[socket.id] = name;
         io.emit('update-users', Object.values(users));
-        socket.emit('video-command', {
-            action: 'initial-sync',
-            url: roomState.url,
-            time: estimatedTime,
-            playing: roomState.playing
-        });
     });
 
-    socket.on('video-command', (data) => {
-        roomState = {
-            url: data.url || roomState.url,
-            time: data.time,
-            playing: (data.action === 'play'),
-            updatedAt: Date.now()
-        };
-        socket.broadcast.emit('video-command', data);
+    // Logika Inti: Broadcast langsung apa adanya
+    socket.on('video-action', (data) => {
+        socket.broadcast.emit('video-action', data);
     });
 
-    socket.on('new-message', (msg) => {
-        socket.broadcast.emit('chat-receive', { user: users[socket.id]?.name || "Anonim", msg });
+    socket.on('new-message', (data) => {
+        socket.broadcast.emit('chat-receive', data);
     });
 
     socket.on('disconnect', () => {
@@ -51,5 +28,4 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
+http.listen(process.env.PORT || 3000, () => console.log("Server Siap!"));
