577dea803a7aaaab661ad9adaa5fc7aaea8d5ddd
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-20 04:23:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,66 +8,63 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-let state = {
+/* ===== SINGLE SOURCE OF TRUTH ===== */
+const state = {
   videoUrl: "",
   time: 0,
-  playing: false
+  playing: false,
+  lastUpdate: Date.now(),
+  users: {} // socketId: { name, status }
 };
-
-let users = {};
 
 io.on("connection", socket => {
 
-  socket.on("join", ({ nickname }) => {
-    if (!nickname) return;
-
-    users[socket.id] = {
-      nickname,
-      status: "watching"
-    };
-
-    socket.emit("sync", { state, users });
-    io.emit("users", users);
+  socket.on("join", name => {
+    state.users[socket.id] = { name, status: "watching" };
+    socket.emit("sync", state);
+    io.emit("users", state.users);
   });
 
-  socket.on("set-video", url => {
+  socket.on("intent-play", () => {
+    state.playing = true;
+    state.lastUpdate = Date.now();
+    io.emit("sync", state);
+  });
+
+  socket.on("intent-pause", () => {
+    state.playing = false;
+    state.lastUpdate = Date.now();
+    io.emit("sync", state);
+  });
+
+  socket.on("intent-seek", time => {
+    state.time = time;
+    state.lastUpdate = Date.now();
+    io.emit("sync", state);
+  });
+
+  socket.on("intent-set-video", url => {
     state.videoUrl = url;
     state.time = 0;
-    state.playing = true;
-    io.emit("set-video", url);
-  });
-
-  socket.on("play", time => {
-    state.playing = true;
-    state.time = time;
-    socket.broadcast.emit("play", time);
-  });
-
-  socket.on("pause", time => {
     state.playing = false;
-    state.time = time;
-    socket.broadcast.emit("pause", time);
+    state.lastUpdate = Date.now();
+    io.emit("sync", state);
   });
 
   socket.on("status", status => {
-    if (users[socket.id]) {
-      users[socket.id].status = status;
-      io.emit("users", users);
+    if (state.users[socket.id]) {
+      state.users[socket.id].status = status;
+      io.emit("users", state.users);
     }
   });
 
-  socket.on("chat", msg => {
-    if (!users[socket.id]) return;
-    io.emit("chat", {
-      nickname: users[socket.id].nickname,
-      msg
-    });
-  });
-
   socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("users", users);
+    delete state.users[socket.id];
+    io.emit("users", state.users);
   });
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () =>
+  console.log("WatchParty Final running on", PORT)
+);
