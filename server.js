2de9fb757de6f469546d072715326ec64c9a3c8d
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-20 03:30:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,57 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(__dirname));
 
 let roomState = {
   src: "",
   time: 0,
-  playing: false
+  playing: false,
+  updatedAt: Date.now()
 };
 
+let users = {};
+
 io.on("connection", socket => {
-  socket.emit("init", roomState);
 
   socket.on("join", name => {
-    socket.name = name;
-    socket.state = "watching";
-    update();
+    users[socket.id] = { name, status: "idle" };
+    socket.emit("init-state", roomState);
+    io.emit("users", users);
   });
 
-  socket.on("video", data => {
-    roomState = { ...roomState, ...data };
+  socket.on("video-action", data => {
+    // ⛔ cegah spam state lama
+    if (Date.now() - roomState.updatedAt < 300) return;
+
+    roomState = {
+      src: data.src ?? roomState.src,
+      time: data.time ?? roomState.time,
+      playing: data.playing ?? roomState.playing,
+      updatedAt: Date.now()
+    };
+
     socket.broadcast.emit("sync", roomState);
   });
 
-  socket.on("state", s => {
-    socket.state = s;
-    update();
-  });
-
-  socket.on("chat", msg => {
-    socket.broadcast.emit("chat", { user: socket.name, msg });
-  });
-
-  socket.on("disconnect", update);
-
-  function update() {
-    const users = [];
-    for (let [, s] of io.sockets.sockets) {
-      if (s.name) users.push({ name: s.name, state: s.state });
+  socket.on("user-status", status => {
+    if (users[socket.id]) {
+      users[socket.id].status = status;
+      io.emit("users", users);
     }
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
     io.emit("users", users);
-  }
+  });
 });
 
-http.listen(process.env.PORT || 3000, () =>
-  console.log("Server ready")
+server.listen(process.env.PORT || 3000, () =>
+  console.log("Nobar server ready")
 );
