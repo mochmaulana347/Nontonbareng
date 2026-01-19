a687d72419d1ba2ccd9a5f63d6db059ee0ec6143
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-19 18:35:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,65 +8,73 @@ app.use(express.static(__dirname));
 let users = {}; 
 let videoStatus = {
     url: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
-    currentTime: 0,
+    lastTime: 0,
+    lastUpdate: Date.now(),
     playing: false,
     adminId: null
 };
 
+// Fungsi menghitung waktu asli video saat ini di server
+function getEstimatedTime() {
+    if (!videoStatus.playing) return videoStatus.lastTime;
+    const elapsed = (Date.now() - videoStatus.lastUpdate) / 1000;
+    return videoStatus.lastTime + elapsed;
+}
+
 io.on('connection', (socket) => {
-    
     socket.on('join-room', (username) => {
         users[socket.id] = { name: username, status: 'Watching', joinedAt: Date.now(), isAdmin: false };
-        
-        const sortedUsers = Object.entries(users).sort((a, b) => a[1].joinedAt - b[1].joinedAt);
-        videoStatus.adminId = sortedUsers[0][0];
-        
-        // Reset semua status admin, lalu set yang baru
+        const sorted = Object.entries(users).sort((a, b) => a[1].joinedAt - b[1].joinedAt);
+        videoStatus.adminId = sorted[0][0];
         Object.keys(users).forEach(id => users[id].isAdmin = (id === videoStatus.adminId));
 
-        io.emit('sys-log', `${username} bergabung!`);
         io.emit('update-users', { users: Object.values(users), adminId: videoStatus.adminId });
 
-        socket.emit('video-control', { action: 'change', url: videoStatus.url });
-        setTimeout(() => {
-            socket.emit('video-control', { action: videoStatus.playing ? 'play' : 'pause', time: videoStatus.currentTime });
-        }, 2000);
-    });
-
-    // FITUR: Ambil alih admin secara manual
-    socket.on('claim-admin', () => {
-        videoStatus.adminId = socket.id;
-        Object.keys(users).forEach(id => users[id].isAdmin = (id === videoStatus.adminId));
-        io.emit('sys-log', `${users[socket.id].name} sekarang menjadi Admin.`);
-        io.emit('update-users', { users: Object.values(users), adminId: videoStatus.adminId });
+        // Kirim status awal
+        socket.emit('video-control', {
+            action: 'change',
+            url: videoStatus.url,
+            time: getEstimatedTime(),
+            playing: videoStatus.playing
+        });
     });
 
     socket.on('video-control', (data) => {
         if (socket.id === videoStatus.adminId) {
-            if (data.url) videoStatus.url = data.url;
+            videoStatus.lastTime = data.time || 0;
+            videoStatus.lastUpdate = Date.now();
             if (data.action === 'play') videoStatus.playing = true;
             if (data.action === 'pause') videoStatus.playing = false;
-            if (data.time !== undefined) videoStatus.currentTime = data.time;
+            if (data.url) videoStatus.url = data.url;
+
             socket.broadcast.emit('video-control', data);
         }
     });
 
+    // Admin lapor posisi secara berkala (Heartbeat)
+    socket.on('heartbeat', (time) => {
+        if (socket.id === videoStatus.adminId) {
+            videoStatus.lastTime = time;
+            videoStatus.lastUpdate = Date.now();
+        }
+    });
+
+    socket.on('claim-admin', () => {
+        videoStatus.adminId = socket.id;
+        Object.keys(users).forEach(id => users[id].isAdmin = (id === videoStatus.adminId));
+        io.emit('update-users', { users: Object.values(users), adminId: videoStatus.adminId });
+    });
+
     socket.on('new-message', (msg) => {
-        socket.broadcast.emit('chat-receive', { user: users[socket.id]?.name || "Anonim", msg: msg });
+        socket.broadcast.emit('chat-receive', { user: users[socket.id]?.name || "Anonim", msg });
     });
 
     socket.on('disconnect', () => {
-        if (users[socket.id]) {
-            delete users[socket.id];
-            const remaining = Object.entries(users).sort((a, b) => a[1].joinedAt - b[1].joinedAt);
-            if (remaining.length > 0) {
-                videoStatus.adminId = remaining[0][0];
-                users[videoStatus.adminId].isAdmin = true;
-            }
-            io.emit('update-users', { users: Object.values(users), adminId: videoStatus.adminId });
-        }
+        delete users[socket.id];
+        const remaining = Object.entries(users).sort((a, b) => a[1].joinedAt - b[1].joinedAt);
+        if (remaining.length > 0) videoStatus.adminId = remaining[0][0];
+        io.emit('update-users', { users: Object.values(users), adminId: videoStatus.adminId });
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => { console.log('Server nyala...'); });
+http.listen(process.env.PORT || 3000);
