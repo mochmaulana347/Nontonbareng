b0aacca865ef28a736e9a850bd0687d19d214125
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-19 17:42:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,39 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
+let users = {}; // Untuk menyimpan data { socketId: username }
+
 io.on('connection', (socket) => {
-    console.log('Seseorang masuk ke room');
+    
+    socket.on('join-room', (username) => {
+        users[socket.id] = username;
+        // Beritahu semua orang ada yang join
+        io.emit('sys-log', `${username} baru saja bergabung!`);
+        // Update daftar user di semua layar
+        io.emit('update-users', Object.values(users));
+    });
 
     socket.on('video-control', (data) => {
         socket.broadcast.emit('video-control', data);
     });
 
     socket.on('new-message', (msg) => {
-        socket.broadcast.emit('chat-receive', msg);
+        socket.broadcast.emit('chat-receive', {
+            user: users[socket.id] || "Anonim",
+            msg: msg
+        });
+    });
+
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            io.emit('sys-log', `${users[socket.id]} keluar dari room.`);
+            delete users[socket.id];
+            io.emit('update-users', Object.values(users));
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('Server nyala di port ' + PORT);
+    console.log('Server berjalan di port ' + PORT);
 });
