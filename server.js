dcda23b46d80667af8ca175cf72f906212a036a7
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-20 09:30:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    // Meneruskan perintah video (play, pause, ganti video)
     socket.on('video-control', (data) => {
         socket.broadcast.emit('video-control', data);
     });
-
-    // Meneruskan pesan chat
     socket.on('new-message', (data) => {
         socket.broadcast.emit('chat-receive', data);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server aktif di port ${PORT}`));
+http.listen(process.env.PORT || 3000);
