e35c398ad205ee84e067d98a271419737f184794
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-20 10:04:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    // Meneruskan perintah video ke semua orang
     socket.on('video-control', (data) => {
         socket.broadcast.emit('video-control', data);
     });
-
-    // Meneruskan chat
     socket.on('new-message', (data) => {
         socket.broadcast.emit('chat-receive', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server Ready di Port ${PORT}`));
+http.listen(PORT, () => console.log(`Server Ready: http://localhost:${PORT}`));
