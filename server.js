4b721961f9f435ff0a90e495a4d5c76cae67dfd4
mochmaulana347 <mochmaulana347@gmail.com>
2026-01-20 11:57:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,33 +8,17 @@ let users = {};
 
 io.on('connection', (socket) => {
     socket.on('join', (name) => {
-        users[socket.id] = { 
-            name, 
-            color: '#' + Math.floor(Math.random()*16777215).toString(16),
-            status: 'active' 
-        };
+        users[socket.id] = { name, color: '#' + Math.floor(Math.random()*16777215).toString(16) };
         io.emit('update-users', Object.values(users));
     });
-
-    socket.on('presence-change', (status) => {
-        if(users[socket.id]) {
-            users[socket.id].status = status;
-            io.emit('update-users', Object.values(users));
-        }
-    });
-
     socket.on('video-control', (data) => { socket.broadcast.emit('video-control', data); });
-    
     socket.on('new-message', (data) => {
-        const user = users[socket.id];
-        socket.broadcast.emit('chat-receive', { ...data, color: user ? user.color : '#e53170' });
+        socket.broadcast.emit('chat-receive', { ...data, color: users[socket.id]?.color });
     });
-
-    socket.on('disconnect', () => { 
-        delete users[socket.id]; 
-        io.emit('update-users', Object.values(users)); 
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('update-users', Object.values(users));
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, '0.0.0.0', () => console.log(`Server Ready`));
+http.listen(process.env.PORT || 3000, () => console.log("Server Ready!"));
